01efe84c606160c89b251e031e2058b9a7ea98c6
Kingsley Omoaka-Iyorah <kingsley.omiy@gmail.com>
2023-04-17 19:01:47 +0100

    nav bar

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav className='header'>
+			<i className="fas fa-bars" aria-hidden="true"></i>
+			<h2 className='header--title'>Ejiye</h2>
+			<i className="fas fa-cart-shopping" aria-hidden="true"></i>
+    </nav>
+  )
+}
+
+export default Header
