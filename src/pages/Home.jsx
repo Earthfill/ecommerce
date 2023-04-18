ea8de02eaeb1102b0859ec7aac7ff3975ea17ba2
Kingsley Omoaka-Iyorah <kingsley.omiy@gmail.com>
2023-04-18 14:40:16 +0100

    home image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,11 @@ const Home = () => {
   return (
     <div className='home'>
       <Header />
+      <img src='/800_5f2db5cf10e66.jpg' className='home--image'/>
+      <div className='home--image--overlay'>
+        <p className="home--text">Fashion & Clothing Store</p>
+        <p className="home--subtext">Extraordinary Collections</p>
+      </div>
     </div>
   )
 }
